932fcf62f69baf8f4f98a5a548130b4f78b667dd
sfjbs <sankalpagrawalcse@gmail.com>
2022-10-06 06:22:15 +0000

    Day01

---
+++ Day01/simple-maven-app/src/main/java/com/rama/data/App.java
@@ -0,0 +1,18 @@
+package com.rama.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Greeting greeting=new Greeting();
+        greeting.sayHello();
+        greeting.sayHi();
+        greeting.sayWelcome();
+        greeting.sayThanks();
+    }
+}
