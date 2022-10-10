5adebe9c2df91ed6ed5926e93e5080f7a4c8721a
sfjbs <sankalpagrawalcse@gmail.com>
2022-10-10 06:25:48 +0000

    Day03

---
+++ Day03/spring-boot-rest/src/test/java/com/sankalp/controller/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankalp.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
