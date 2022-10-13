21fe876e3f15e451becf22640b924967acc762fd
sfjbs <sankalpagrawalcse@gmail.com>
2022-10-13 08:07:24 +0000

    Day06 student exam result

---
+++ Day06/student-exam-result/src/main/java/com/sankalp/app/StudentController.java
@@ -0,0 +1,43 @@
+package com.sankalp.app;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class StudentController {
+
+    private Map<String,Student> studentMap= new HashMap<String,Student>();
+
+    @GetMapping("/get/student")
+    public Student getStudent(@RequestParam String name){
+        Student result= studentMap.get(name);
+        return result;
+    }
+
+    @GetMapping("/get/all")
+    public Map<String,Student> getStudent(){
+        return studentMap;
+    }
+
+    @PostMapping("/save/student")
+    public void SaveStudent(@RequestBody Student student){
+        String name= student.getName();
+        studentMap.put(name,student);
+    }
+
+    @PutMapping("/update/student")
+    public Student updateStudent(@RequestParam String name,@RequestParam String schoolName){
+        Student result= studentMap.get(name);
+        result.setSchoolName(schoolName);
+        studentMap.put(name,result);
+        return result;
+
+    }
+
+    @DeleteMapping("/remove/student")
+    public void deleteStudent(@RequestParam String name){
+        studentMap.remove(name);
+    }
+}
---
+++ Day06/student-exam-result/src/test/java/com/sankalp/app/StudentExamResultApplicationTests.java
@@ -0,0 +1,106 @@
+package com.sankalp.app;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest({StudentController.class})
+@ActiveProfiles(value = "test")
+public class StudentExamResultApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Value("${student.get.url}")
+	String geturl;
+
+	@Value("${student.get.all.url}")
+	String getallurl;
+
+	@Value("${student.post.url}")
+	String posturl;
+
+	@Value("${student.put.url}")
+	String puturl;
+
+	@Value("${student.delete.url}")
+	String deleteurl;
+
+	@Test
+	public void testStudentGet() throws Exception {
+		ResultActions responseEntity  = mockMvc.perform(get(geturl).param("name","stud1"));
+		responseEntity.andExpect(status().isOk());
+		String result = responseEntity.andReturn().getResponse().getContentAsString();
+		assertEquals("", result);
+	}
+
+	@Test
+	public void testStudentWithValueGet() throws Exception {
+		Student student = new Student("stud1", "roll1", "xyz1", 70, 71, 72, 73, 74);
+		mockMvc.perform(post(posturl).contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(student)).accept(MediaType.APPLICATION_JSON));
+
+		ResultActions responseEntity = mockMvc.perform(get(geturl).param("name", "stud1"));
+		responseEntity.andExpect(status().isOk());
+		String result = responseEntity.andReturn().getResponse().getContentAsString();
+		assertNotNull(result);
+	}
+
+		@Test
+		public void testAllStudentGet() throws Exception {
+			ResultActions responseEntity  = mockMvc.perform(get(getallurl));
+			responseEntity.andExpect(status().isOk());
+			String result = responseEntity.andReturn().getResponse().getContentAsString();
+			assertNotNull(result);
+		}
+
+	@Test
+	public void testSaveStudentPost() throws Exception {
+		Student student=new Student("name1","roll1","xyz1",70,71,72,73,74);
+		ResultActions responseEntity  = mockMvc.perform(post(posturl).contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(student)).accept(MediaType.APPLICATION_JSON));
+		responseEntity.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testUpdateStudentPut() throws Exception {
+		Student student=new Student("name1","roll1","xyz1",70,71,72,73,74);
+		mockMvc.perform(post(posturl).contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(student)).accept(MediaType.APPLICATION_JSON));
+
+		ResultActions responseEntity  = mockMvc.perform(put(puturl).param("name","name1")
+				.param("schoolName","std3"));
+		responseEntity.andExpect(status().isOk());
+		String result = responseEntity.andReturn().getResponse().getContentAsString();
+		assertNotNull(result);
+
+	}
+
+	private String asJsonString(final Object obj) {
+		try {
+			final ObjectMapper mapper = new ObjectMapper();
+			final String jsonContent = mapper.writeValueAsString(obj);
+			return jsonContent;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	}
+
+
